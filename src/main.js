d825e6e37302ab0a36567e3551816b1538bf6390
SantosAdan <santos.adaan@gmail.com>
2017-06-20 22:03:20 -0300

    Fim do Curso Básico

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import {Team} from './team'
+import _ from 'lodash'
 
 require("style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css")
 require('bootstrap')
@@ -7,6 +8,12 @@ require('bootstrap')
 let meuVue = new Vue({
   el: '#app',
   data: {
+    order: {
+      keys: ['score', 'scoredGoals', 'concededGoals'],
+      sort: ['desc', 'desc', 'asc']
+    },
+    columns: ['Nome', 'score', 'scoredGoals', 'concededGoals', 'Saldo'],
+    filter: '',
     teams: [
       new Team('Palmeiras', require('./assets/palmeiras_60x60.png')),
       new Team('Flamengo', require('./assets/flamengo_60x60.png')),
@@ -38,7 +45,8 @@ let meuVue = new Vue({
         team: null,
         goals: 0
       }
-    }
+    },
+    view: 'results'
   },
   methods: {
     endGame () {
@@ -47,15 +55,48 @@ let meuVue = new Vue({
           guestGoals = +this.newGame.guest.goals
 
       this.newGame.home.team.endGame(guestTeam, guestGoals, homeGoals)
+
+      this.showView('results')
+    },
+    createNewGame () {
+      // Criamos a nova partida randomicamente
+      let indexHome = Math.floor(Math.random() * 20),
+          indexGuest = Math.floor(Math.random() * 20)
+
+      // Iniciamos o placar como 0 x 0
+      this.newGame.home.team = this.teams[indexHome]
+      this.newGame.home.goals = 0
+      this.newGame.guest.team = this.teams[indexGuest]
+      this.newGame.guest.goals = 0
+
+      this.showView('newGame')
+    },
+    /**
+     * Altera a view que será exibida para a de novo jogo
+     * TODO: utilizar esse tipo de coisa com vuex e components
+     */
+    showView (viewName) {
+      this.view = viewName
+    },
+    /**
+     * Ordena a tabela de resultados pela coluna selecionada
+     * @param  String columnName 
+     */
+    sortBy (columnName) {
+      this.order.keys = columnName
+      this.order.sort = this.order.sort == 'desc' ? 'asc' : 'desc'
     }
   },
-  created () {
-    let indexHome = Math.floor(Math.random() * 20),
-        indexGuest = Math.floor(Math.random() * 20)
+  computed: {
+    teamsFiltered() {
+      let collection = _.orderBy(this.teams, this.order.keys, this.order.sort)
 
-    this.newGame.home.team = this.teams[indexHome]
-    this.newGame.home.goals = 0
-    this.newGame.guest.team = this.teams[indexGuest]
-    this.newGame.guest.goals = 0
+      return _.filter(collection, item => item.name.indexOf(this.filter) >= 0)
+    }
+  },
+  filters: {
+    balance (team) {
+      return team.scoredGoals - team.concededGoals
+    }
   }
 })
